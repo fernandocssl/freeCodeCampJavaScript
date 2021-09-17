b2ae5795b39363236673693c3cc3738ad8422252
Fernando César Silva <78764087+fernandocssl@users.noreply.github.com>
2021-09-17 19:15:27 -0300

    Adding new files

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,21 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  //Create an object below
+  {
+    "artist": "Charli XCX",
+    "title": "How I Am Feeling Now",
+    "release_year": 2020,
+    "formats": ["CD", "LP"]
+
+  }
+];
